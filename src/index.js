8a4aee39a609c2dc14e6b5777add3f4fefe4ed1a
Ross Evans <therealrossevans@gmail.com>
2018-07-10 08:53:28 -0400

    Remove use of service workers

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,4 +12,4 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root'),
 );
-registerServiceWorker();
+unregister();
